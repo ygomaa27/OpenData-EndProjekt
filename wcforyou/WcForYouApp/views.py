68cfffad2703d58dc552829340cbe87bd356819c
Paul Hassanpour <n.paul.h@hotmail.com>
2021-05-04 12:33:33 +0200

    get data down

--- wcforyou/WcForYouApp/views.py
+++ wcforyou/WcForYouApp/views.py
@@ -4,9 +4,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import json, requests
 
-#Https request the data
-data = requests.get('https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:WCANLAGE2OGD&srsName=EPSG:4326&outputFormat=json').json()
-
 #include our index
 def index(request):
-    return render(request, 'WcForYouApp/index.html')
+    #Https request the data 
+    #Path https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:WCANLAGE2OGD&srsName=EPSG:4326&outputFormat=json
+    data = requests.get('https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:WCANLAGE2OGD&srsName=EPSG:4326&outputFormat=json').json()
+    return render(request, 'WcForYouApp/index.html', context = { 'data': data})
