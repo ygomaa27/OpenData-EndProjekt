c311390bf502c269490c6c3647effc7599019e06
Paul Hassanpour <n.paul.h@hotmail.com>
2021-05-04 12:20:04 +0200

    get json data & include leaflet

--- wcforyou/WcForYouApp/views.py
+++ wcforyou/WcForYouApp/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+import json, requests
 
+#Https request the data
+data = requests.get('https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:WCANLAGE2OGD&srsName=EPSG:4326&outputFormat=json').json()
 
+#include our index
 def index(request):
     return render(request, 'WcForYouApp/index.html')
